198b8a54db266334baf77e1b979c0310b38e7c21
Jakub Melka <Mgr.Jakub.Melka@gmail.com>
2023-07-02 09:01:51 +0200

    Fix compilation error on linux

--- Pdf4QtLib/sources/pdfutils.h
+++ Pdf4QtLib/sources/pdfutils.h
@@ -733,6 +733,8 @@ private:
     std::vector<ClosedInterval> m_intervals;
 };
 
+QDataStream& operator>>(QDataStream& stream, long unsigned int &i);
+
 template<typename T>
 QDataStream& operator>>(QDataStream& stream, std::vector<T>& vector)
 {
@@ -758,8 +760,6 @@ QDataStream& operator<<(QDataStream& stream, const std::vector<T>& vector)
     }
     return stream;
 }
-
-QDataStream& operator>>(QDataStream& stream, long unsigned int &i);
 
 template<typename T, size_t Size>
 QDataStream& operator>>(QDataStream& stream, std::array<T, Size>& array)
