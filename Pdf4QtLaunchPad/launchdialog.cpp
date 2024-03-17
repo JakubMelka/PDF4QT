ccd69e1ac48ecba49392099935f34bd26ee0e917
Jakub Melka <Mgr.Jakub.Melka@gmail.com>
2024-03-17 14:36:24 +0100

    Issue #159: Compilation fix

--- Pdf4QtLaunchPad/launchdialog.cpp
+++ Pdf4QtLaunchPad/launchdialog.cpp
@@ -30,28 +30,44 @@ LaunchDialog::LaunchDialog(QWidget* parent)
     ui->setupUi(this);
     ui->retranslateUi(this);
 
-    ui->appEditorWidget->getButton()->setIcon(QIcon(":/app-editor.svg"));
-    ui->appViewerWidget->getButton()->setIcon(QIcon(":/app-viewer.svg"));
-    ui->appDiffWidget->getButton()->setIcon(QIcon(":/app-diff.svg"));
-    ui->appPageMasterWidget->getButton()->setIcon(QIcon(":/app-pagemaster.svg"));
+    LaunchApplicationWidget* appEditorWidget = new LaunchApplicationWidget(this);
+    appEditorWidget->setObjectName("appEditorWidget");
+    ui->gridLayout->addWidget(appEditorWidget, 0, 0);
 
-    ui->appEditorWidget->getTitleLabel()->setText(tr("Editor"));
-    ui->appViewerWidget->getTitleLabel()->setText(tr("Viewer"));
-    ui->appDiffWidget->getTitleLabel()->setText(tr("Diff"));
-    ui->appPageMasterWidget->getTitleLabel()->setText(tr("PageMaster"));
+    LaunchApplicationWidget* appViewerWidget = new LaunchApplicationWidget(this);
+    appViewerWidget->setObjectName("appViewerWidget");
+    ui->gridLayout->addWidget(appViewerWidget, 0, 1);
 
-    ui->appEditorWidget->getDescriptionLabel()->setText(tr("Go beyond basic browsing. This tool packs a punch with a host of advanced features, including encryption, document reading, digital signature verification, annotation editing, and even support for searching text using regular expressions. Turn pages into images, and enhance your PDF interactions with multiple available plugins."));
-    ui->appViewerWidget->getDescriptionLabel()->setText(tr("Simplify your viewing experience. This lightweight viewer offers essential viewing functions in a clean, user-friendly interface."));
-    ui->appDiffWidget->getDescriptionLabel()->setText(tr("Spot differences effortlessly. This tool allows users to open two documents and receive a detailed list of differences. View these differences in a page-to-page window where they are clearly marked. Save these differences into an XML file for future reference."));
-    ui->appPageMasterWidget->getDescriptionLabel()->setText(tr("Take control of your documents. Manage whole documents or individual pages with ease. Merge documents into a single file, or split them into multiple ones. You can also move, clone, or add pages with a few clicks, all within an intuitive user interface."));
+    LaunchApplicationWidget* appDiffWidget = new LaunchApplicationWidget(this);
+    appDiffWidget->setObjectName("appDiffWidget");
+    ui->gridLayout->addWidget(appDiffWidget, 1, 0);
 
-    ui->appEditorWidget->getButton()->setDefault(true);
-    ui->appEditorWidget->getButton()->setFocus();
+    LaunchApplicationWidget* appPageMasterWidget = new LaunchApplicationWidget(this);
+    appPageMasterWidget->setObjectName("appPageMasterWidget");
+    ui->gridLayout->addWidget(appPageMasterWidget, 1, 1);
 
-    connect(ui->appEditorWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startEditor);
-    connect(ui->appViewerWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startViewer);
-    connect(ui->appDiffWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startDiff);
-    connect(ui->appPageMasterWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startPageMaster);
+    appEditorWidget->getButton()->setIcon(QIcon(":/app-editor.svg"));
+    appViewerWidget->getButton()->setIcon(QIcon(":/app-viewer.svg"));
+    appDiffWidget->getButton()->setIcon(QIcon(":/app-diff.svg"));
+    appPageMasterWidget->getButton()->setIcon(QIcon(":/app-pagemaster.svg"));
+
+    appEditorWidget->getTitleLabel()->setText(tr("Editor"));
+    appViewerWidget->getTitleLabel()->setText(tr("Viewer"));
+    appDiffWidget->getTitleLabel()->setText(tr("Diff"));
+    appPageMasterWidget->getTitleLabel()->setText(tr("PageMaster"));
+
+    appEditorWidget->getDescriptionLabel()->setText(tr("Go beyond basic browsing. This tool packs a punch with a host of advanced features, including encryption, document reading, digital signature verification, annotation editing, and even support for searching text using regular expressions. Turn pages into images, and enhance your PDF interactions with multiple available plugins."));
+    appViewerWidget->getDescriptionLabel()->setText(tr("Simplify your viewing experience. This lightweight viewer offers essential viewing functions in a clean, user-friendly interface."));
+    appDiffWidget->getDescriptionLabel()->setText(tr("Spot differences effortlessly. This tool allows users to open two documents and receive a detailed list of differences. View these differences in a page-to-page window where they are clearly marked. Save these differences into an XML file for future reference."));
+    appPageMasterWidget->getDescriptionLabel()->setText(tr("Take control of your documents. Manage whole documents or individual pages with ease. Merge documents into a single file, or split them into multiple ones. You can also move, clone, or add pages with a few clicks, all within an intuitive user interface."));
+
+    appEditorWidget->getButton()->setDefault(true);
+    appEditorWidget->getButton()->setFocus();
+
+    connect(appEditorWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startEditor);
+    connect(appViewerWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startViewer);
+    connect(appDiffWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startDiff);
+    connect(appPageMasterWidget->getButton(), &QPushButton::clicked, this, &LaunchDialog::startPageMaster);
 
     setFixedSize(minimumSizeHint());
 }
